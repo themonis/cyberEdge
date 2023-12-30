86117e2605a24e789866415d03a18851461fad82
themonis <141899472+themonis@users.noreply.github.com>
2023-12-30 10:14:10 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      FontFamily: {
+        mullish: ["Mullish"]
+      },
+      colors: {
+        deepBlue: "#111927",
+
+        lightBlue: "#2b84ea",
+
+        lightBlue300: "#4b94ed",
+
+        lightBlue500: "#0b72e7",
+        skyBlue: "#a4b1cd",
+        greenLight: "#9fef00",
+        grayText: "#818597",
+
+        lightGray: "#e2e2e2",
+        grayBlue: "#344a6c",
+        deepBlueHead: "#162f56",
+        CardsColor: "#1a2332",
+      }
+    },
+  },
+  plugins: [],
+}
+
